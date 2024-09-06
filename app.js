f3d915f9403222b53b8017998ecc156d952847ff
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2024-09-06 01:28:28 -0300

    atualizando o js e adicionando partido e propostas

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-console.log(dados[0].nome);
+function pesquisar() {  
+  let section = document.getElementById('resultados-pesquisa');
+
+  let resultados = "";
+
+  for(let dado of dados) {
+    resultados += `
+          <div class="item-resultado">
+            <h2><a href="${dado.link_wikipedia}">${dado.nome} - (${dado.partido})</a></h2>
+            <h3>Mini bio</h3>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.proposta}" target="_blank" class = "link_proposta">Propostas</a>
+          </div>`
+  }
+
+  section.innerHTML = resultados;
+}
+
 
--- dados.js
+++ dados.js
@@ -2,21 +2,24 @@ let Boulos = {
   nome: "Guilherme Boulos",
   descricao: "Conhecido pela participação no Movimento dos Trabalhadores Sem-Teto (MTST), Guilherme Boulos, 41 anos, é formado em filosofia pela USP e mestre em psiquiatria pela Faculdade de Medicina da USP. Em 2018, ingressou na política como candidato à presidência da República, filiado ao PSOL.",
   partido: "PSOL",
-  link_wikipedia: "https://pt.wikipedia.org/wiki/Guilherme_Boulos"
+  link_wikipedia: "https://pt.wikipedia.org/wiki/Guilherme_Boulos",
+  proposta: "https://exame.com/brasil/eleicoes-em-sp-o-que-pensa-guilherme-boulos-sobre-saude-habitacao-educacao-e-seguranca-publica/"
 };
 
 let Marcal = {
   nome: "Pablo Marçal",
   descricao: "Pablo Marçal, 34 anos, é formado em direito pela PUC-SP e mestre em direito pela USP. Atualmente, é vereador na cidade de São Paulo, eleito pelo Partido Novo.",
   partido: "NOVO",
-  link_wikipedia: "https://pt.wikipedia.org/wiki/Pablo_Mar%C3%A7al"
+  link_wikipedia: "https://pt.wikipedia.org/wiki/Pablo_Mar%C3%A7al",
+  proposta: "https://exame.com/brasil/eleicoes-em-sp-o-que-pensa-pablo-marcal-sobre-saude-habitacao-educacao-e-seguranca-publica/"
 };
 
 let Datena = {
   nome: "José Luiz Datena",
   descricao: "José Luiz Datena, 63 anos, é jornalista e apresentador de televisão. Atualmente, é filiado ao PSL e exerce o cargo de deputado federal por São Paulo.",
   partido: "PSL",
-  link_wikipedia: "https://pt.wikipedia.org/wiki/Datena"
+  link_wikipedia: "https://pt.wikipedia.org/wiki/Datena",
+  proposta: "https://exame.com/brasil/eleicoes-em-sp-o-que-pensa-datena-sobre-saude-habitacao-educacao-e-seguranca-publica/"
 };
 
 let dados = [
