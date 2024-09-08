f4605dc6b8c0ce352221f4729ff27176422dddc8
gustavolhonda <gustavo.honda@estudante.ufscar.br>
2024-09-08 19:49:33 -0300

    Terminando as aulas

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
 function pesquisar() {  
   let section = document.getElementById('resultados-pesquisa');
 
+  let campoPesquisa = document.getElementById('campo-pesquisa').value;
+
+  if (campoPesquisa.trim() === "") {
+    section.innerHTML = "Nada foi encontrado. Você deve digitar o nome ou partido do candidato.";
+    return;
+  }
+
+  campoPesquisa = campoPesquisa.toLowerCase();
+
   let resultados = "";
 
   for(let dado of dados) {
-    resultados += `
+    let nome = dado.nome.toLowerCase();
+    let descricao = dado.descricao.toLowerCase();
+    let partido = dado.partido.toLowerCase();
+    let tags = dado.tags.toLowerCase();
+
+    if (nome.includes(campoPesquisa) ||  partido.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+      resultados += `
           <div class="item-resultado">
             <h2><a href="${dado.link_wikipedia}">${dado.nome} - (${dado.partido})</a></h2>
             <h3>Mini bio</h3>
             <p class="descricao-meta">${dado.descricao}</p>
-            <a href="${dado.proposta}" target="_blank" class = "link_proposta">Propostas</a>
-          </div>`
-  }
+            <a href="${dado.proposta}" target="_blank" class = "link_proposta ">Propostas</a>
+          </div>`;  
+    } 
+
+    if (!resultados) {
+      resultados = "Nada foi encontrado. Tente novamente.";
+    }
 
   section.innerHTML = resultados;
 }
-
+}
 
--- dados.js
+++ dados.js
@@ -3,7 +3,8 @@ let Boulos = {
   descricao: "Conhecido pela participação no Movimento dos Trabalhadores Sem-Teto (MTST), Guilherme Boulos, 41 anos, é formado em filosofia pela USP e mestre em psiquiatria pela Faculdade de Medicina da USP. Em 2018, ingressou na política como candidato à presidência da República, filiado ao PSOL.",
   partido: "PSOL",
   link_wikipedia: "https://pt.wikipedia.org/wiki/Guilherme_Boulos",
-  proposta: "https://exame.com/brasil/eleicoes-em-sp-o-que-pensa-guilherme-boulos-sobre-saude-habitacao-educacao-e-seguranca-publica/"
+  proposta: "https://exame.com/brasil/eleicoes-em-sp-o-que-pensa-guilherme-boulos-sobre-saude-habitacao-educacao-e-seguranca-publica/",
+  tags: ""
 };
 
 let Marcal = {
@@ -11,7 +12,8 @@ let Marcal = {
   descricao: "Pablo Marçal, 34 anos, é formado em direito pela PUC-SP e mestre em direito pela USP. Atualmente, é vereador na cidade de São Paulo, eleito pelo Partido Novo.",
   partido: "NOVO",
   link_wikipedia: "https://pt.wikipedia.org/wiki/Pablo_Mar%C3%A7al",
-  proposta: "https://exame.com/brasil/eleicoes-em-sp-o-que-pensa-pablo-marcal-sobre-saude-habitacao-educacao-e-seguranca-publica/"
+  proposta: "https://exame.com/brasil/eleicoes-em-sp-o-que-pensa-pablo-marcal-sobre-saude-habitacao-educacao-e-seguranca-publica/",
+  tags: " "
 };
 
 let Datena = {
